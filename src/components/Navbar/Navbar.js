93369b89e4c0ee92869bcb5d26b08ad71c4e8888
Julio Añoveros <julio.ab@outlook.com>
2022-03-28 19:28:27 -0600

    WIP: Navbar layout

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,19 +4,9 @@ import './Navbar.css'
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <form className="navbar__search-form">
-        <input
-          className="navbar__search-input"
-          type="text"
-          placeholder="Search"
-        />
-        <button className="navbar__search-button" type="submit">
-          Search
-        </button>
-      </form>
       <div className="navbar__items">
         <a className="navbar__item" href="#">
-          Home 
+          Home
         </a>
         <a className="navbar__item" href="#">
           About
@@ -25,6 +15,14 @@ export default function Navbar() {
           Contact
         </a>
       </div>
+      <form action="#" method="GET" className="navbar__search-form">
+        <ion-icon name="search-outline"></ion-icon>
+          <input
+          className="navbar__search-input"
+          type="text"
+          placeholder="Search"
+        />
+      </form>
     </nav>
   );
 }
